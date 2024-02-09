3ad713055c48a28d414a6e204e7b607a49b4c9b0
thisura-D <thisuradealwis@gmail.com>
2024-02-09 19:46:07 +0530

    customerView done

--- src/pos/mvc/PosMvc.java
+++ src/pos/mvc/PosMvc.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package pos.mvc;
-
+import pos.mvc.view.CustomerView;
 /**
  *
  * @author Thisura
@@ -15,6 +15,7 @@ public class PosMvc {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        new CustomerView().setVisible(true);
     }
     
 }
